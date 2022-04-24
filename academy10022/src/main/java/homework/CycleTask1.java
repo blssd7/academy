5b588879d7e636155f5373d4df20a4f26f2bf5ba
Renata Valevach <renatavalevach@gmail.com>
2022-04-24 20:44:52 +0300

    very

---
+++ academy10022/src/main/java/homework/CycleTask1.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class CycleTask1 {
+
+	public static void main(String[] args) {
+		int x = Integer.parseInt("53");
+		if (x % 2 != 0) {
+
+			System.out.println(x);
+		}
+	}
+}
